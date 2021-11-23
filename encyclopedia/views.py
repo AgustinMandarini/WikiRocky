9ac3204956fb363c70864c34e9eec73e1bc43e55
Chwaksen <agustinmandarini47@gmail.com>
2021-11-23 18:28:19 -0300

    My First Commit

---
+++ encyclopedia/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from . import util
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def title(request, title):
+	if util.get_entry(title) == None:
+		return render(request, "encyclopedia/404.html")
+	return render(request, "encyclopedia/title.html", {
+		"title": util.get_entry(title), "page_title": title
+	})
